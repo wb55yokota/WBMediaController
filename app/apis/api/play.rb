fd7b3b88b7d9e686e90bcd97048569659b088e1a
yokota <wb55yokota@gmail.com>
2015-03-08 01:08:34 +0900

    use mkfifo

--- app/apis/api/play.rb
+++ app/apis/api/play.rb
@@ -2,12 +2,11 @@ class API::Play < Grape::API
   resource :play do
     desc 'play file'
     post do
+      PIPE = '/tmp/omxpipe'
       dir = params[:dir].presence ? params[:dir] : '/'
-      threads = []
-      threads << Thread.new do
-        Omx
-        exec("omxplayer " + dir + Shellwords.escape(params[:filename]))
-      end
+      system "mkfifo #{PIPE}" unless File.exists?(PIPE)
+      system "omxplayer " + "\"" + dir + params[:filename] + "\" < #{PIPE} &"
+      system "echo -n start > #{PIPE}"
       { :status=>1, :error=>nil, :result=>{} }
     end
   end
